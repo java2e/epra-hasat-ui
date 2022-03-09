91239b173696147a710376ad9585d0b9f5e4ad9f
mustafa.yogun <musyoo@gmail.com>
2022-03-09 20:05:40 +0300

    #ReactivePower created.

--- src/environments/ApiPath.js
+++ src/environments/ApiPath.js
@@ -1,5 +1,6 @@
 export const apiPath = {
 API_BASE_PATH: 'http://ec2-52-59-241-21.eu-central-1.compute.amazonaws.com:80/api',
+//API_BASE_PATH: 'http://localhost:84/api',
 APP_BASE_PATH: 'http://localhost:4200/apps',
 APP_ROOT_PATH: '/apps',
 
@@ -12,6 +13,8 @@ GETALL:'/getAll',
 COMPANY: '/company',
 USER   : '/user',
 FEEDER : '/feeder',
-FEEDER_USER_PATH: '/feederUserPath'
+FEEDER_USER_PATH: '/feederUserPath',
+PV_LOCATION: '/pvLocation',
+REACTIVE_POWER : '/reactivePower'
 
 }
