a836bd96f559dbf6c02a9900d7e480a2333ba244
Mehmet Demircioglu <demircioglumhmt@gmail.com>
2022-04-27 22:08:33 +0300

    production configuration

--- src/environments/ApiPath.js
+++ src/environments/ApiPath.js
@@ -1,6 +1,7 @@
 export const apiPath = {
-API_BASE_PATH: 'http://ec2-52-59-241-21.eu-central-1.compute.amazonaws.com:80/api',
+//API_BASE_PATH: 'http://ec2-52-59-241-21.eu-central-1.compute.amazonaws.com:80/api',
 //API_BASE_PATH: 'http://localhost:80/api',
+API_BASE_PATH : 'http://192.168.1.108:8181/api',
 APP_BASE_PATH: 'http://localhost:3000/apps',
 APP_ROOT_PATH: '/apps',
 }
