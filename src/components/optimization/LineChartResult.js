9f3cbd04fdf78d736d67184a43db77c9acd6acac
mustafayogun <musyoo@gmail.com>
2022-05-28 19:12:49 +0300

    LineChartResult added

--- src/components/optimization/LineChartResult.js
+++ src/components/optimization/LineChartResult.js
@@ -4,10 +4,10 @@ import * as am5xy from "@amcharts/amcharts5/xy";
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
  
 
-const LineChart=(props)=> {
+const LineChartResult=(props)=> {
   useLayoutEffect(() => {
     
-    console.log(props.data);
+    console.log(props);
     let root = am5.Root.new("chartdiv");
     
 
@@ -50,13 +50,15 @@ const LineChart=(props)=> {
       };
     }
 
-    function generateData2(i,data) {
+    function generateData1(i,data) {
 
         return {
           date: i,
           value: data
         };
       }
+
+     
     
     function generateDatas(count) {
       let data = [];
@@ -70,14 +72,26 @@ const LineChart=(props)=> {
 
     function generateDatasActivePowerGross(list) {
         let data = [];
-          
-        if(list.voltage) {
-        for (var i = 0; i < list.voltage.length; ++i) {
-          data.push(generateData2(i+1,list.voltage[i]));
+          debugger
+        if(list.voltageTrue) {
+        for (var i = 0; i < list.voltageTrue.length; ++i) {
+          data.push(generateData1(i+1,list.voltageTrue[i]));
         }
     }
         return data;
       }
+
+      function generateDatasFalse(list) {
+        let data = [];
+          debugger
+        if(list.voltageFalse) {
+        for (var i = 0; i < list.voltageFalse.length; ++i) {
+          data.push(generateData1(i+1,list.voltageFalse[i]));
+        }
+    }
+        return data;
+      }
+
 
   
     // Create axes
@@ -122,7 +136,19 @@ const LineChart=(props)=> {
     // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
     let series = chart.series.push(am5xy.LineSeries.new(root, {
       stroke: am5.color(0x6794dc),
-      name: "Şebeke Gerilim Grafiği",
+      name: "Mevcut Şebeke Gerilim Grafiği",
+      xAxis: xAxis,
+      yAxis: yAxis,
+      valueYField: "value",
+      valueXField: "date",
+      tooltip: am5.Tooltip.new(root, {
+        labelText: "{valueY}"
+      })
+    }));
+
+    let series2 = chart.series.push(am5xy.LineSeries.new(root, {
+      stroke: am5.color(0xff0000),
+      name: "Optimum Şebeke Gerilim Grafiği",
       xAxis: xAxis,
       yAxis: yAxis,
       valueYField: "value",
@@ -141,9 +167,11 @@ const LineChart=(props)=> {
     }));
     
     let data = generateDatas(50000);
-    let activePowerGross = generateDatasActivePowerGross(props.data);
+    let activePowerGross = generateDatasActivePowerGross(props.voltageTrueList);
+    let activePowerGross2 = generateDatasFalse(props.voltageFalseList);
     series.data.setAll(activePowerGross);
-
+    series2.data.setAll(activePowerGross2);
+debugger
     
 
     let legend = chart.children.push(am5.Legend.new(root, {
@@ -157,7 +185,7 @@ const LineChart=(props)=> {
     // Make stuff animate on load
     // https://www.amcharts.com/docs/v5/concepts/animations/
     series.appear(1000);
-   
+    series2.appear(1000);
 
 
 
@@ -173,4 +201,4 @@ const LineChart=(props)=> {
     <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
   );
 }
-export default LineChart;
+export default LineChartResult;
